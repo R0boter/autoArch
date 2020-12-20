19cf6228dc5e334d86a04f56a672316ad3efce73
falcon <3433538213@qq.cm>
2020-12-20 11:24:24 +0800

    重构安装脚本

---
+++ baseInstall.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+# install some base programs
+pacman -S which git wget curl unrar unzip tar gcc make fontconfig --noconfirm
+wait
+
+# set time zone
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+hwclock --systohc
+
+# set locale
+sed -i 's/#\(en_US.UTF-8\)/\1/' /etc/locale.gen
+sed -i 's/#\(zh_CN.UTF-8\)/\1/' /etc/locale.gen
+sed -i 's/#\(zh_CN.GBK\)/\1/' /etc/locale.gen
+locale-gen
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+
+# set hostname Archlinux is your hostname
+read -p "What's hostname you want use?(default is Archlinux)" hostname
+if [ ! -n "$hostname" ];then
+    echo 'Archlinux' > /etc/hostname
+else
+    echo $hostname > /etc/hostname
+fi
+
+# set locale hosts file
+cat >> /etc/hosts <<EOF
+
+# GitHub Start
+52.74.223.119 github.com
+192.30.253.119 gist.github.com
+54.169.195.247 api.github.com
+185.199.111.153 assets-cdn.github.com
+151.101.76.133 raw.githubusercontent.com
+151.101.108.133 user-images.githubusercontent.com
+151.101.76.133 gist.githubusercontent.com
+151.101.76.133 cloud.githubusercontent.com
+151.101.76.133 camo.githubusercontent.com
+151.101.76.133 avatars0.githubusercontent.com
+151.101.76.133 avatars1.githubusercontent.com
+151.101.76.133 avatars2.githubusercontent.com
+151.101.76.133 avatars3.githubusercontent.com
+151.101.76.133 avatars4.githubusercontent.com
+151.101.76.133 avatars5.githubusercontent.com
+151.101.76.133 avatars6.githubusercontent.com
+151.101.76.133 avatars7.githubusercontent.com
+151.101.76.133 avatars8.githubusercontent.com
+# GitHub End
+EOF
+
+# set nvim editor
+ln -s /usr/bin/nvim /usr/bin/vi
+ln -s /usr/bin/nvim /usr/bin/vim
+
+# enable pacman color
+sed -i 's/#Color/Color/g' /etc/pacman.conf
+sed -i '/Color/a\ILoveCandy' /etc/pacman.conf
+
+# install boot, I used archlinux built-in bootstrap
+bootctl --path=/boot install
+wait
+
+cat > /boot/loader/loader.conf <<EOF
+#timeout 3
+#console-mode keep
+default arch
+EOF
+
+cat > /boot/loader/entries/arch.conf <<EOF
+title    Arch Linux
+linux   /vmlinuz-linux
+initrd  /initramfs-linux.img
+options root=/dev/sda2 rw
+EOF
+
+# set root passwd
+read -p "Please set root password(default is toor)：" rpass
+if [ ! -n "$rpass" ];then
+    echo "root:toor" | chpasswd
+else
+    echo "root:$rpass" | chpasswd
+fi
+
+# creat normal user username and set passwd
+read -p "Please set your username：" uname
+read -p "Please set your password：" upass
+useradd -m -g users -s /usr/bin/zsh -G wheel,uucp $uname && echo "$uname:$upass" | chpasswd
+
+# set sudo file
+sed -i '/NOPASSWD/s/^#\ //' /etc/sudoers
+
+# set autologin
+mkdir /etc/systemd/system/getty@tty1.service.d/
+cat > /etc/systemd/system/getty@tty1.service.d/override.conf <<EOF
+[Service]
+ExecStart=
+ExecStart=-/usr/bin/agetty --autologin username --noclear %I \$TERM
+EOF
+
+# set network
+read -p "Do you have a wifi interface?[y/n](default:y): " chk
+if [[ -z $chk || $chk == "y" || $chk == "Y" ]];then
+    pacman -S wpa_supplicant bluez bluez-utils --noconfirm
+    cat > /etc/systemd/network/25-wireless.network <<EOF
+[Match]
+Name=wl*
+[Network]
+DHCP=ipv4
+EOF
+    cat > /etc/wpa_supplicant/wpa_supplicant.conf <<EOF
+ctrl_interface=/var/run/wpa_supplicant
+ctrl_interface_group=wheel
+update_config=1
+fast_reauth=1
+ap_scan=1
+EOF
+    cat <<EOF
+Now the wireless configuration complete and configfile is /etc/systemd/network/25-wireless.network
+And wpa_supplicant generates WIFI configuration file is /etc/wpa_supplicant/wpa_supplicant.conf
+When you want to connect to WIFI，
+You should use command 'wpa_passphrase <ssid> [passphrase] >> /etc/wpa_supplicant/wpa_supplicant.conf' to auto configuration!
+Use command 'wpa_supplicant -B -i <interface> -c /etc/wpa_supplicant/wpa_supplicant.conf' to connect internet!!!"
+EOF
+    read -p "Please remember these files location!!! Press any key......"
+fi
+cat > /etc/systemd/network/20-wired.network <<EOF
+[Match]
+Name=en*
+[Network]
+DHCP=ipv4
+EOF
+systemctl enable systemd-networkd
+
+cat <<EOF
+Now the wire-interface configuration complete and configfile is /etc/systemd/network/20-wired.network
+If you have your own DHCP server，you need change the .network configfile，and enable systemd-resolved.service
+EOF
+read -p "Please remember this file location!!! Press any key......"
+
+# set DNS server in /etc/resolv.conf
+sed -i '$a nameserver 114.114.114.114' /etc/resolv.conf
+sed -i '$a nameserver 8.8.8.8' /etc/resolv.conf
+
+# read -p "Do you want to connect to WIFI?[y/n](default:y):  " wifi_chk
+# if [[ -z $wifi_chk || $wifi_chk == "y" || $wifi_chk == "Y" ]];then
+#     read -p "Please enter your wifi-name：  " wifiname
+#     read -p "Please enter your wifi-password：  " wifipasswd
+#     while [[ -z $wifiname || -z $wifipasswd ]]
+#     do
+#         echo "You need enter the wifiname and wifipasswd!!!"
+#         read -p "Please enter your wifi-name：  " ssid
+#         read -p "Please enter your wifi-password：  " passphrase
+#     done
+
+#     arch_chroot "wpa_passphrase ${ssid} ${passphrase} >> /etc/wpa_supplicant/wpa_supplicant.conf"
+# fi
+# Add archlinucn sources,The default source is USTC
+cat >> /etc/pacman.conf <<EOF
+[archlinuxcn]
+SigLevel = Optional TrustedOnly
+Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch
+#Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch
+#Server = http://mirrors.163.com/archlinux-cn/$arch
+#Server = http://repo.archlinuxcn.org/$arch
+EOF
---
+++ startInstall.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+timedatectl set-ntp true
+# check your boot mode, this script only supports uefi boot mode
+ls /sys/firmware/efi/efivars >/dev/null 2>&1
+if [ $? -eq 0 ]
+then
+    echo "Your system is boot with UEFI! It's great!!!"
+    read -p "Let's start your archlife!!!，If your are not ready，you can use ctrl-c to end!!!"
+
+    read -p "Are you sure format your sda[y/n]: " sure_str
+    if [[ $sure_str != "y" && $sure_str != "Y" ]]
+    then
+        exit 0
+    fi
+    # format your disk
+    printf "y\n" | mkfs.ext4 /dev/sda
+    printf "n\n1\n\n+512M\nef00\nw\ny\n" | gdisk /dev/sda && yes | mkfs.fat -F32 /dev/sda1
+    printf "n\n2\n\n\n8300\nw\ny\n" | gdisk /dev/sda && yes | mkfs.ext4 /dev/sda2
+    mount /dev/sda2 /mnt && mkdir /mnt/boot && mount /dev/sda1 /mnt/boot
+    # config your mirror source file
+    mv -f /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+    curl -Lo mirrorlist https://www.archlinux.org/mirrorlist/?country=CN&protocol=http&protocol=https&ip_version=4&ip_version=6
+    wait
+    sed -i 's/^#Server/Server/g' ./mirrorlist
+    mv -f ./mirrorlist /etc/pacman.d/mirrorlist
+    chmod 644 /etc/pacman.d/mirrorlist
+    # Install the base system
+    pacstrap /mnt base linux linux-firmware sudo zsh neovim
+    wait
+    # creat fstab file
+    genfstab -U /mnt >> /mnt/etc/fstab
+    # root change to /mnt
+    arch-chroot /mnt /bin/bash -c "/baseInstall.sh"
+else
+    echo "Sorry! Please use UEFI to boot your system!!!"
+    exit 0
+fi
