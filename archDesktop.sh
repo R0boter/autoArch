42c9a562615896939164220bc3455195613d5503
Falcon <3433538213@qq.com>
2021-12-13 01:32:51 +0800

    add archlinux-keyring

--- archDesktop.sh
+++ archDesktop.sh
@@ -31,7 +31,7 @@ function cfg_processor(){
 }
 
 function cfg_desktop(){
-    sudo pacman -S xorg-server xorg-xinit xclip xorg-xsetroot xorg-xrandr alsa-utils v2ray pkgconf --noconfirm
+    sudo pacman -S xorg-server xorg-xinit xclip xorg-xsetroot xorg-xrandr alsa-utils v2ray pkgconf archlinuxcn-keyring --noconfirm
     for i in `seq 5`;do
         sudo sed -i '$d' /etc/X11/xinit/xinitrc
     done
